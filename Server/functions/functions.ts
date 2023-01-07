03f8a478b3713a16baad88373edc5be9b23c507d
Xasanboy <hasanbojabdurasulov309@gmail.com>
2023-01-07 21:37:05 +0500

    Finish

--- Server/functions/functions.ts
+++ Server/functions/functions.ts
@@ -102,40 +102,110 @@ export const teachersEdit = async (req: Request, res: Response) => {
     try {
         const { id } = req.params
         const { name, lastname, email, password, country, degree, img } = req.body
+        const ids = (await pool.query(`SELECT * FROM teachers WHERE name = $1 AND email = $2 AND password = $3`, [name, email, password])).rows
         const user = (await pool.query(`SELECT * FROM teachers WHERE id = $1`, [id])).rows[0]
-        if (id !== undefined) {
-            pool.query(`UPDATE teachers SET name = $1, lastname = $2,email = $3,password = $4,country=$5,degree=$6,img=$7`, [
-                name !== undefined ? name : user.name,
-                lastname !== undefined ? lastname : user.lastname,
-                email !== undefined ? email : user.email,
-                password !== undefined ? password : user.password,
-                country !== undefined ? country : user.country,
-                degree !== undefined ? degree : user.degree,
-                img !== undefined ? img : user.img
-            ])
-            res.status(200).json({ message: `Your teacher with id ${id} has edited!` })
+        if (ids.length == 0) {
+            if (id !== undefined) {
+                pool.query(`UPDATE teachers SET name = $1, lastname = $2,email = $3,password = $4,country=$5,degree=$6,img=$7 WHERE id = $8`, [
+                    name !== undefined ? name : user.name,
+                    lastname !== undefined ? lastname : user.lastname,
+                    email !== undefined ? email : user.email,
+                    password !== undefined ? password : user.password,
+                    country !== undefined ? country : user.country,
+                    degree !== undefined ? degree : user.degree,
+                    img !== undefined ? img : user.img,
+                    id
+                ])
+                res.status(200).json({ message: `Your teacher with id ${id} has edited!` })
+            }
+        } else {
+            res.status(200).json({ message: "You can not edit your data at this time!" })
         }
     } catch (error: any) {
         res.status(500).json({ message: error.message })
     }
 }
-
-export const pupilsEdit = async (req: Request, res: Response) => {
-    try {
-    } catch (error: any) {
-        res.status(500).json({ message: error.message })
-    }
-}
-
+// 
 export const headteacherEdit = async (req: Request, res: Response) => {
     try {
-        const { name, lastname, email, password, country, clas, img } = req.body
+        const { id } = req.params
+        const uds = (await pool.query(`SELECT * FROM headtteacher WHERE id = $1`, [id])).rows[0]
+        const { name, lastname, email, password, country, degree, img } = req.body
         if (name !== undefined && email !== undefined && password !== undefined) {
-            const pupil = pool.query(`SELECT * FROM pupils WHERE name = $1 AND email = $2 AND password = $3`, [name, email, password])
-        }else{
-            
+            const ids = (await pool.query(`SELECT * FROM headteacher WHERE name = $1 AND email = $2 AND password = $3`, [name, email, password])).rows
+            if (ids.length == 0) {
+                pool.query(`UPDATE headteacher SET name = $1,lastname=$2,email=$3,password=$4,country=$5,degree=$6,img=$7 WHERE  id = $8`, [
+                    name !== undefined ? name : uds.name,
+                    lastname !== undefined ? lastname : uds.lastname,
+                    email !== undefined ? email : uds.email,
+                    password !== undefined ? password : uds.password,
+                    country !== undefined ? country : uds.country,
+                    degree !== undefined ? degree : uds.degree,
+                    img !== undefined ? img : uds.img,
+                    id
+                ])
+                res.status(200).json({ message: "Edited succesfully" })
+            } else {
+                res.status(200).json({ message: "You can edit your data at this time!" })
+            }
+        } else {
+            pool.query(`UPDATE headteacher SET name = $1,lastname=$2,email=$3,password=$4,country=$5,degree=$6,img=$7 WHERE  id = $8`, [
+                name !== undefined ? name : uds.name,
+                lastname !== undefined ? lastname : uds.lastname,
+                email !== undefined ? email : uds.email,
+                password !== undefined ? password : uds.password,
+                country !== undefined ? country : uds.country,
+                degree !== undefined ? degree : uds.degree,
+                img !== undefined ? img : uds.img,
+                id
+            ])
+            res.status(200).json({ message: "Edited succesfully" })
         }
     } catch (error: any) {
-
+        res.status(500).json({ message: error.message })
+    }
+}
+// 
+export const pupilsEdit = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const ids = (await pool.query(`SELECT * FROM headteacher WHERE id = $1`, [id])).rows[0]
+        const { name, lastname, email, password, country, clas, img } = req.body
+        if (name !== undefined && email !== undefined && password !== undefined) {
+            const pupil = (await pool.query(`SELECT * FROM pupils  WHERE name = $1 AND email = $2 AND password = $3`, [name, email, password])).rows
+            if (pupil.length == 0) {
+                await pool.query(`UPDATE pupils SET name = $1, lastname = $2, email = $3, password = $4, country=$5,class=$6,img=$7,id = $8`, [
+                    name !== undefined ? name : ids.name,
+                    lastname !== undefined ? lastname : ids.lastname,
+                    email !== undefined ? email : ids.email,
+                    password !== undefined ? password : ids.password,
+                    country !== undefined ? country : ids.country,
+                    clas !== undefined ? country : ids.country,
+                    img !== undefined ? img : ids.img,
+                    id
+                ])
+                return res.status(200).json({ message: "Your account has edited succesfully!" })
+            } else {
+                res.status(400).json({ message: "You can't set your data at this time!" })
+            }
+        } else {
+            if (id !== undefined) {
+                pool.query(`UPDATE pupils SET name = $1, lastname = $2, email = $3, password = $4, country=$5,class=$6,img=$7 WHERE id = $8 `, [
+                    name !== undefined ? name : ids.name,
+                    lastname !== undefined ? lastname : ids.lastname,
+                    email !== undefined ? email : ids.email,
+                    password !== undefined ? password : ids.password,
+                    country !== undefined ? country : ids.country,
+                    clas !== undefined ? country : ids.country,
+                    img !== undefined ? img : ids.img,
+                    id
+                ])
+                return res.status(200).json({ message: "Edited succesfully!" })
+            } else {
+                res.status(400).json({ message: "You can't set your data at this time!" })
+            }
+        }
+    } catch (error: any) {
+        res.status(500).json({ message: error.message })
     }
 }
--- Server/server.ts
+++ Server/server.ts
@@ -1,8 +1,22 @@
 import express, { Request, Response } from "express"
-import { getMethod, postMethod, deleteMethod, teachersEdit,headteacherEdit,pupilsEdit } from "./functions/functions"
+import { getMethod, postMethod, deleteMethod, teachersEdit, headteacherEdit, pupilsEdit } from "./functions/functions"
 const PORT = 8080
 const server = express()
+server.use(express.urlencoded({ extended: true }))
+server.set("view engine", "ejs")
 server.use(express.json())
+
+
+// Get Default
+server.get('/register', (req: Request, res: Response) => {
+    try {
+        res.render("index", { title: "Working.....?", letter: "Hello World" })
+    } catch (error: any) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+
 // Get
 server.get("/:id", getMethod)
 
@@ -14,8 +28,9 @@ server.delete("/:id", deleteMethod)
 
 // Edit
 server.put("/teachers/:id", teachersEdit)
-server.put("/headteachers/:id",headteacherEdit)
-server.put("/pupils/:id",pupilsEdit)
+server.put("/headteachers/:id", headteacherEdit)
+server.put("/pupils/:id", pupilsEdit)
+
 server.listen(PORT, () => {
     console.log(`Server: http://localhost:${PORT}`)
 })
