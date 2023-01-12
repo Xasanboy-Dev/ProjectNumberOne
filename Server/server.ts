97c197b4adbcb26993376b999e38517f0240c2a8
Ilyas729 <ilyas.reimbaev94@gmail.com>
2023-01-12 14:06:12 +0500

    another

--- Server/server.ts
+++ Server/server.ts
@@ -7,20 +7,32 @@ server.use(express.json())
 server.use(cors())
 server.use(express.urlencoded({ extended: true }))
 
-
-
-
-
 server.get('/', (req: Request, res: Response) => {
     try {
-        res.status(200).json({ message: "Hello Bro!" })
+        res.status(200).json({ message: "Hello Bro" })
     } catch (error: any) {
         res.status(500).json({ message: error.message })
     }
 })
+// Post
 
-
-
+server.post("/users", async (req: Request, res: Response) => {
+    try {
+        const { name, lastname, email, password } = req.body
+        if (name !== undefined && lastname !== undefined && email !== undefined && password !== undefined) {
+            const user = (await pool.query(`SELECT * FROM users WHERE name = $1 AND lastname = $2 AND email = $3 AND password = $4`, [name, lastname, email, password])).rows
+            if (user.length == 0) {
+                (await pool.query(`INSERT INTO users (name,lastname,email,password) VALUES ($1,$2,$3,$4)`, [name, lastname, email, password])).rows
+            } else {
+                res.status(204).json({ message: "You have already registered!" })
+            }
+        } else {
+            res.status(200).json({ message: "You must fill all th gaps!" })
+        }
+    } catch (error: any) {
+        res.status(500).json({ message: error.message })
+    }
+})
 
 server.listen(PORT, () => {
     console.log(`Server: http://localhost:${PORT}`)
