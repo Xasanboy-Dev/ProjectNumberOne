17306c497a10dd3a1eb42ef9db73eb7c87cffb75
Xasanboy <hasanbojabdurasulov309@gmail.com>
2023-01-26 21:05:00 +0500

    new

--- Server/config/db.ts
+++
@@ -1,8 +0,0 @@
-import { Pool } from "pg"
-export const pool = new Pool({
-   database: "postgres",
-   host: "localhost",
-   port: 5432,
-   user: "postgres",
-   password: "1234"
-});
--- svelte/src/routes/country.svelte
+++
@@ -1,7 +0,0 @@
-<option>Uzbekistan</option>
-<option>Russia</option>
-<option>English</option>
-<option>Japan</option>
-<option>Korea</option>
-<option>Arabic</option>
-<option>Turkmenistan</option>
